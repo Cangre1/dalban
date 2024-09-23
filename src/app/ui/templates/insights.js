9ae59301cb3dbf7a7a83392e8e064d8f0039e52c
Cangre1 <suareznicolasm@gmail.com>
2024-09-23 11:48:42 -0300

    update Nico

--- src/app/ui/templates/insights.js
+++ src/app/ui/templates/insights.js
@@ -30,15 +30,24 @@ const Insights = ({ data }) => {
               }}
             >
               <div className="absolute bottom-0 left-0 w-full text-white p-5 sm:p-10 space-y-2 sm:space-y-4">
-                <span className="block text-base sm:text-lg">
-                  {insights.grid[0].category}
-                </span>
-                <h1 className="text-xl sm:text-2xl font-bold">
-                  {insights.grid[0].title}
-                </h1>
-                <p className="text-base sm:text-lg">
-                  {insights.grid[0].paragraph}
-                </p>
+                <span
+                  className="block text-base sm:text-lg"
+                  dangerouslySetInnerHTML={{
+                    __html: insights.grid[0].category,
+                  }}
+                ></span>
+                <h1
+                  className="text-xl sm:text-2xl font-bold"
+                  dangerouslySetInnerHTML={{
+                    __html: insights.grid[0].title,
+                  }}
+                ></h1>
+                <p
+                  className="text-sm sm:text-base"
+                  dangerouslySetInnerHTML={{
+                    __html: insights.grid[0].paragraph,
+                  }}
+                ></p>
               </div>
             </div>
 
@@ -50,17 +59,58 @@ const Insights = ({ data }) => {
               }}
             >
               <div className="absolute bottom-0 left-0 w-full text-white p-5 sm:p-10 space-y-2 sm:space-y-4">
-                <span className="block text-base sm:text-lg">
-                  {insights.grid[1].category}
-                </span>
-                <h1 className="text-xl sm:text-2xl font-bold">
-                  {insights.grid[1].title}
-                </h1>
+                <span
+                  className="block text-base sm:text-lg"
+                  dangerouslySetInnerHTML={{
+                    __html: insights.grid[1].category,
+                  }}
+                ></span>
+                <h1
+                  className="text-xl sm:text-2xl font-bold"
+                  dangerouslySetInnerHTML={{
+                    __html: insights.grid[1].title,
+                  }}
+                ></h1>{" "}
+                <p
+                  className="text-sm sm:text-base"
+                  dangerouslySetInnerHTML={{
+                    __html: insights.grid[1].paragraph,
+                  }}
+                ></p>
+              </div>
+            </div>
+
+            {/* Tercer elemento de la grilla */}
+            <div
+              className="bg-cover bg-center py-24 sm:py-32 relative  shadow-lg hover:shadow-2xl transition-all ease-in-out duration-300 transform hover:-translate-y-1"
+              style={{
+                backgroundImage: `linear-gradient(to top, #0099A8 10%, rgba(0,0,0,0) 50%), url(${insights.grid[1].src})`,
+              }}
+            >
+              <div className="absolute bottom-0 left-0 w-full text-white p-5 sm:p-10 space-y-2 sm:space-y-4">
+                <span
+                  className="block text-base sm:text-lg"
+                  dangerouslySetInnerHTML={{
+                    __html: insights.grid[2].category,
+                  }}
+                ></span>
+                <h1
+                  className="text-xl sm:text-2xl font-bold"
+                  dangerouslySetInnerHTML={{
+                    __html: insights.grid[2].title,
+                  }}
+                ></h1>{" "}
+                <p
+                  className="text-sm sm:text-base"
+                  dangerouslySetInnerHTML={{
+                    __html: insights.grid[2].paragraph,
+                  }}
+                ></p>
               </div>
             </div>
 
             {/* Grilla anidada para los dos últimos elementos */}
-            <div className="col-start-1 sm:col-start-2">
+            <div className="col-start-1 sm:col-start-2 hidden">
               <div className="grid grid-cols-2 gap-3">
                 {/* Tercer elemento de la grilla */}
                 <div
@@ -87,12 +137,18 @@ const Insights = ({ data }) => {
                   }}
                 >
                   <div className="absolute bottom-0 left-0 w-full text-white p-5 sm:p-10 space-y-2 sm:space-y-4">
-                    <span className="block text-base sm:text-lg">
-                      {insights.grid[3].category}
-                    </span>
-                    <h1 className="text-xl sm:text-2xl font-bold">
-                      {insights.grid[3].title}
-                    </h1>
+                    <span
+                      className="block text-base sm:text-lg"
+                      dangerouslySetInnerHTML={{
+                        __html: insights.grid[3].category,
+                      }}
+                    ></span>
+                    <h1
+                      className="text-xl sm:text-2xl font-bold"
+                      dangerouslySetInnerHTML={{
+                        __html: insights.grid[3].title,
+                      }}
+                    ></h1>
                   </div>
                 </div>
               </div>
