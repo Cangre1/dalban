3771ca1a63eb416f9a446d67783a1e610d0bfd6d
Cangre1 <suareznicolasm@gmail.com>
2024-11-21 12:58:04 -0300

    update Nico

--- src/app/logistica/page.js
+++ src/app/logistica/page.js
@@ -32,6 +32,16 @@ export default function Logistics() {
           <ServicesBanner data={data.characteristics} />
           <PhraseBanner data={data} />
           <Map data={data.map} />
+          <div className="pt-5 bg-[#252969] ">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6579.8201125761525!2d-58.702373!3d-34.454431!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bca2493d06fbc7%3A0x5e470f4eff7cb2e8!2sDalban%20Pharma!5e0!3m2!1ses!2sar!4v1732204497706!5m2!1ses!2sar"
+              className="w-full h-[500px]"
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+
           <InformationBanner data={data} />
         </>
       )}
