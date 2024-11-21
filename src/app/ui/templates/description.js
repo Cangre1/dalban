e24a8145ca0c0df2ef24f794788ae75b1d5e1bf9
Cangre1 <suareznicolasm@gmail.com>
2024-11-21 12:19:00 -0300

    Update description.js

--- src/app/ui/templates/description.js
+++ src/app/ui/templates/description.js
@@ -4,7 +4,7 @@ const Description = ({ data }) => {
     <div id="nosotros" className="contenedor-custom !py-12 lg:!py-28">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-y-5 lg:gap-y-0">
         {/* Columna del título */}
-        <div data-aos="zoom-in">
+        <div>
           <h1
             className="titles"
             dangerouslySetInnerHTML={{ __html: aboutUs.title }}
@@ -12,7 +12,7 @@ const Description = ({ data }) => {
         </div>
 
         {/* Columna del párrafo */}
-        <div data-aos="zoom-in">
+        <div>
           <p
             className="paragraphs"
             dangerouslySetInnerHTML={{ __html: aboutUs.paragraph }}
